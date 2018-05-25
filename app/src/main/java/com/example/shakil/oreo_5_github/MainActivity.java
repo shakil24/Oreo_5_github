6ffc8bf5250337c16bbad6ba2d24308af3416096
shakil24 <32729170+shakil24@users.noreply.github.com>
2018-05-25 16:31:53 +0600

    toast implemented

--- app/src/main/java/com/example/shakil/oreo_5_github/MainActivity.java
+++ app/src/main/java/com/example/shakil/oreo_5_github/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.shakil.oreo_5_github;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void press (View view)
+    {
+        Toast.makeText(MainActivity.this,"Button has been pressed!!", Toast.LENGTH_LONG).show();
+
     }
 }
